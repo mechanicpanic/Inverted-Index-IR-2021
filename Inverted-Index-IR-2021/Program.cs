e7767b0ece485d2117f74f1799f5c3c6348a769b
Anna <voidlambda@gmail.com>
2021-03-08 16:18:50 +0300

    Добавьте файлы проекта.

---
+++ Inverted-Index-IR-2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inverted_Index_IR_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
